6caca7b7ddb51fc49fc95ef917764faefd0e6241
satackey <satackeys@gmail.com>
2019-07-18 10:35:07 +0900

    課題の修正

--- p88prog3-3.c
+++ p88prog3-3.c
@@ -5,36 +5,34 @@ int main(void) {
     short is_last_return = 1;
     short is_number_typing = 0;
     int price = 0;
-    short rate = 100;
+    short rate = 120;
     while ((in = getchar()) != '\n') {
         switch (in) {
             case 'T':
-                // putchar(' ');
-                // putchar(' ');
-                printf("  ");
+                putchar('\t');
                 is_last_return = 0;
                 break;
             
             case 'N':
-                printf("\n");
+                putchar('\n');
                 is_last_return = 1;
                 break;
             
             case '+':
-                printf("%d", price * rate);
-                printf(" ");
+                printf("%05d", price * rate);
+                putchar(' ');
                 is_last_return = 0;
                 is_number_typing = 0;
                 break;
             
             case '$':
-                printf("\\");
+                putchar('\\');
                 is_number_typing = 1;
                 price = 0;
                 break;
 
             case '-':
-                printf("%d", price * rate);
+                printf("%05d", price * rate);
                 is_number_typing = 0;
                 // -はそのまま出力するので break しない
 
