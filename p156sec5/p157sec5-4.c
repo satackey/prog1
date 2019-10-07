f5e32c334635c1e163933ec874aae893337575e3
satackey <21271711+satackey@users.noreply.github.com>
2019-10-07 09:29:48 +0900

    残りもの

---
+++ p156sec5/p157sec5-4.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(void)
+{
+    char str1[10], str2[10];
+
+    while (1) {
+        printf("文字列を2つ入力してください: ");
+        // int result = 
+
+        if (scanf("%s %s", str1, str2) == EOF) {
+            printf("\n処理を終了します\n");
+            break;
+        }
+
+        int equal = 1;
+        for (int i = 0; i < 10; i++) {
+            if (str1[i] == '?' || str2[i] == '?') {
+                continue;
+            }
+
+            if (str2[i] != str2[i]) {
+                equal = 0;
+            }
+        }
+
+        if (equal) {
+            printf("文字列は一致");
+        } else {
+            printf("文字列は不一致");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
