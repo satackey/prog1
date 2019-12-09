ade6d1bedb2bd50e458edbc17b34f9bba6976dda
satackey <21271711+satackey@users.noreply.github.com>
2019-12-09 09:32:42 +0900

    sizeof

--- p203chap7sec1/p206program7-1.c
+++ p203chap7sec1/p206program7-1.c
@@ -8,5 +8,9 @@ int main(void)
     printf("%d+%ld = %ld\n", data_a, data_b, data_a + data_b);
     data_c = data_a + data_b ;
     printf("%d+%ld = %d\n", data_a, data_b, data_c);
+
+    printf("sizeof(data_a) = %ldバイト\n", sizeof(data_a));
+    printf("sizeof(data_b) = %ldバイト\n", sizeof(data_b));
+    printf("sizeof(data_c) = %ldバイト\n", sizeof(data_c));
     return 0;
 }
