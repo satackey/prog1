3bbbf879f1585a168638355e80e9b5fc92db9d7a
satackey <21271711+satackey@users.noreply.github.com>
2019-12-09 09:40:02 +0900

    sizeof

--- p203chap7sec1/p207program7-2.c
+++ p203chap7sec1/p207program7-2.c
@@ -10,5 +10,10 @@ int main(void)
     printf("キャスト演算子を使用した            :%f\n", kekka);
     kekka = (double)(suuchi_1 / suuchi_2);
     printf("キャスト演算子を全体の括弧に使用した:%f\n", kekka);
+
+
+    printf("sizeof(suuchi_1) = %ldバイト\n", sizeof(suuchi_1));
+    printf("sizeof(suuchi_2) = %ldバイト\n", sizeof(suuchi_2));
+    printf("sizeof(kekka) = %ldバイト\n", sizeof(kekka));
     return 0;
 }
