6e3a2edb40ab297bfad9ff5569e188beea31c670
satackey <21271711+satackey@users.noreply.github.com>
2019-12-09 09:38:14 +0900

    キャスト演算子

---
+++ p203chap7sec1/p207program7-2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+    int suuchi_1 = 100, suuchi_2 = 3;
+    double kekka;
+
+    kekka = suuchi_1 / suuchi_2;
+    printf("キャスト演算子を使用しない          :%f\n", kekka);
+    kekka = (double)suuchi_1 / suuchi_2;
+    printf("キャスト演算子を使用した            :%f\n", kekka);
+    kekka = (double)(suuchi_1 / suuchi_2);
+    printf("キャスト演算子を全体の括弧に使用した:%f\n", kekka);
+}
