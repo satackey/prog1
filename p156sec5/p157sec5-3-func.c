17c292fe094c98053688a17634d9822863e61d11
satackey <21271711+satackey@users.noreply.github.com>
2019-12-09 09:24:58 +0900

    前回の残り

---
+++ p156sec5/p157sec5-3-func.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(void)
+{
+    for (int col_i = 0; col_i < 10; col_i++) {
+        for (int row_i = 0; row_i < 10 - col_i; row_i++) {
+            printf("%d\t", pas_t[col_i][row_i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+void pas(int pas_t[][10]) {
+
+    int pas_t[10][10];
+
+    // 初期化
+    for (int i = 0; i < 10; i++) {
+        pas_t[0][i] = 1;
+        pas_t[i][0] = 1;
+    }
+
+    for (int col_i = 1; col_i < 10; col_i++) {
+        for (int row_i = 1; row_i < 10 - col_i; row_i++) {
+            pas_t[col_i][row_i] = pas_t[col_i-1][row_i] + pas_t[col_i][row_i-1];
+        }
+    }
+
+    return pas_t;
+}
--- p156sec5/p157sec5-4.c
+++ p156sec5/p157sec5-4.c
@@ -1,37 +1,48 @@
 #include<stdio.h>
+#include<string.h>
+#include<math.h>
 
+int check(char str1[10], char str2[10]);
 int main(void)
 {
     char str1[10], str2[10];
 
     while (1) {
         printf("文字列を2つ入力してください: ");
-        // int result = 
 
         if (scanf("%s %s", str1, str2) == EOF) {
             printf("\n処理を終了します\n");
             break;
         }
 
-        int equal = 1;
-        for (int i = 0; i < 10; i++) {
-            if (str1[i] == '?' || str2[i] == '?') {
-                continue;
-            }
-
-            if (str2[i] != str2[i]) {
-                equal = 0;
-                break;
-            }
-        }
-
-        if (equal) {
-            printf("文字列は一致");
-        } else {
-            printf("文字列は不一致");
-        }
         printf("\n");
+
+        int equal = check(str1, str2);
+        if (equal) {
+            printf("文字列は一致\n");
+        } else {
+            printf("文字列は不一致\n");
+        }
     }
 
     return 0;
+}
+
+int check(char str1[], char str2[]) {
+    int equal = 1;
+    for (int i = 0; i < max(strlen(str1), strlen(str2)); i++) {
+        if (str1[i] == '?' || str2[i] == '?') {
+            continue;
+        }
+
+        if (str1[i] != str2[i]) {
+            equal = 0;
+            break;
+        }
+
+        if (str1[i] == '\0' && str2[i] == '\0') {
+            break;
+        }
+    }
+    return equal;
 }
