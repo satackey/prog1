6adbf9537d86ab70f0574d5cc9739c66cc35bb4c
satackey <21271711+satackey@users.noreply.github.com>
2019-12-23 10:34:37 +0900

    コミットしないで実習室を出て行ったさたけくんへbyたにの

--- p224chap7programming/p225chap7programming7-3.c
+++ p224chap7programming/p225chap7programming7-3.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-unsigned short change(char[17]);
-unsigned short str_to_bits(char[17]);
+unsigned short change(unsigned char[17]);
+unsigned short str_to_bits(unsigned char[17]);
 
 int main(void) {
     char input_str[17];
     printf("模範解答を入力してください: ");
     scanf("%s", input_str);
+    change(input_str);
 
     while (scanf("%s", input_str) != EOF) {
         unsigned short result = change(input_str);
@@ -16,32 +17,34 @@ int main(void) {
     return 0;
 }
 
-unsigned short change(char string[17]) {
+unsigned short change(unsigned char string[17]) {
     static short has_registered = 0;
-    static unsigned short mohan = -1;
-    if (has_registered == 1) {
+    static unsigned short mohan = 0;
+    if (has_registered != 1) {
         mohan = str_to_bits(string);
+        has_registered = 1;
         return 0;
     }
 
     unsigned short kaitou = str_to_bits(string);
-    unsigned short result_bits = mohan & kaitou;
-        printf("%d\n", kaitou);
-        printf("%d\n", result_bits);
+    unsigned short result_bits = mohan ^ kaitou;
+    printf("m%d\n", mohan);
+    printf("k%d\n", kaitou);
+    printf("r%d\n", result_bits);
 
-    unsigned short ok_count = 0;
+    unsigned short ok_count = 16;
     for (int i = 0; i < 16; i++) {
-        unsigned short check_bit = 1 << i;
-        printf("%d\n", check_bit & result_bits);
-        if ((check_bit & result_bits) > 0) {
-            ok_count++;
+        if (result_bits & 1) {
+            ok_count--;
         }
+        printf("%d\n", result_bits);
+        result_bits >>= 1;
     }
 
     return ok_count;
 }
 
-unsigned short str_to_bits(char string[17]) {
+unsigned short str_to_bits(unsigned char string[17]) {
     unsigned short bits;
     for (int i = 0; i < strlen(string); i++) {
         bits += string[i] == '1' ? 1 : 0;
